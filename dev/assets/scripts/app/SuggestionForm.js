956e17b1acd35c0800eb4804392355dba05c1b57
djeanlouis <danielo.jean-louis@digitaslbi.fr>
2016-10-04 18:03:05 +0200

    update suggest form

--- dev/assets/scripts/app/SuggestionForm.js
+++ dev/assets/scripts/app/SuggestionForm.js
@@ -2,16 +2,45 @@ import 'babel-polyfill';
 import React from 'react';
 
 import styles from './../../stylesheets/form.css';
+import button from './../../stylesheets/button.css';
 
 export default class SuggestionContainer extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  _onSumbit(e) {
+    e.preventDefault();
+
+    let formData = {};
+    
+    [].slice.call(e.target.elements).forEach(function(element) {
+      if (element.name) {
+        formData[element.name] = element.value;
+      }
+    });
+
+    if (!formData.name || formData.name == '') {
+      return;
+    }
+
+    let queryString = '';
+    for (let key in formData) {
+      queryString += `${key}=${formData[key]}&`;
+    }
+    
+    fetch(`${window.boBaseURL}/suggestion/create`, {
+        method: 'POST',
+        body: {}
+      })
+      .then(response => response.json())
+      .then(function(json) {
+      }.bind(this))
+  }
 
   render() {
     return (
-      <form className={styles.form} name="suggest">
+      <form className={styles.form} name="suggest" onSubmit={(e) => this._onSumbit(e) }>
         <legend>Suggérer un restaurant</legend>
         <fieldset>
           <label>Nom du restaurant *</label>
@@ -19,16 +48,22 @@ export default class SuggestionContainer extends React.Component {
         </fieldset>
         <fieldset>
           <label>Description</label>
-          <textarea name='description' value='This is a description.' />
+          <textarea name='description' placeholder='Description' />
         </fieldset>
         <fieldset>
           <label>Adresse</label>
-          <input name='adresse' placeholder='Nom du restaurant' />
+          <input name='adresse' placeholder='Adresse (rue + code postal)' />
         </fieldset>
         <fieldset>
           <label>Budget</label>
-          <input name='adresse' placeholder='Budget' type='number' step='1' min='0' max='10' value='5' />
+          <input name='budget' placeholder='Budget' type='number' step='1' min='0' max='10' defaultValue='5' />
         </fieldset>
+        <fieldset>
+          <button style={{marginTop: 'auto'}}
+          type="submit"
+          className={[button.reset, button.cta, button.fullwidth].join(' ')}>Envoyer ma suggestion</button>
+        </fieldset>
+
       </form>
     )
   }
